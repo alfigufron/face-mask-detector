fc8dbce1a83ab8a8fb28e9e32ccc73d80ab5ebd4
M Alfi Gufron <alfigufron21@gmail.com>
2020-10-22 16:35:09 +0700

    face detector fix

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from mtcnn import MTCNN
-import cv2
+import cv2, os
 
-detector = MTCNN()
+cascPath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_alt2.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 videoCapture = cv2.VideoCapture(0)
 
 while (True):
   ret, frame = videoCapture.read()
-  frame = cv2.resize(frame, (600, 400))
-  boxes = detector.detect_faces(frame)
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(60, 60),
+    flags=cv2.CASCADE_SCALE_IMAGE)
 
-  if boxes:
-    box = boxes[0]['box']
-    conf = boxes[0]['confidence']
-    x, y, w, h = box[0], box[1], box[2], box[3]
-
-    if conf > 0.5:
-      cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
+  for (x, y, w, h) in faces:
+    cv2.rectangle(
+      frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
   
-  cv2.imshow("Frame", frame)
+  cv2.imshow("Face Detector", frame)
   if cv2.waitKey(25) & 0XFF == ord('q'):
     break
 
