abfc6a0c4397da9442156d002a6bac494c84fb93
M Alfi Gufron <alfigufron21@gmail.com>
2020-10-27 11:11:50 +0700

    clear main

--- main.py
+++ main.py
@@ -1,27 +1,59 @@
-from mtcnn import MTCNN
-import cv2, os
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
-cascPath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_alt2.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-videoCapture = cv2.VideoCapture(0)
+import numpy as np
+import os, cv2
 
-while (True):
-  ret, frame = videoCapture.read()
+path = f"{os.path.dirname(cv2.__file__)}/data/haarcascade_frontalface_alt2.xml"
+face_cascade = cv2.CascadeClassifier(path)
+
+model = load_model('./model.h5')
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+  ret, frame = video_capture.read()
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  faces = faceCascade.detectMultiScale(
+
+  face = face_cascade.detectMultiScale(
     gray,
     scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(60, 60),
-    flags=cv2.CASCADE_SCALE_IMAGE)
+    minSize=(10, 10),
+    flags=cv2.CASCADE_SCALE_IMAGE
+  )
 
-  for (x, y, w, h) in faces:
-    cv2.rectangle(
-      frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
+  face_list = []
+  predicts = []
+
+  for (x, y, w, h) in face:
+    face_frame = frame[y:y+h, x:x+w]
+    face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
+    face_frame = cv2.resize(face_frame, (224, 224))
+    face_frame = img_to_array(face_frame)
+    face_frame = np.expand_dims(face_frame, axis=0)
+    face_frame = preprocess_input(face_frame)
+    face_list.append(face_frame)
+
+    if len(face_list) > 0:
+      predicts = model.predict(face_list)
+    
+    for predict in predicts:
+      (mask, without_mask) = predict
+
+    label = "Menggunakan Masker" if mask > without_mask else "Tanpa Masker"
+    color = (0, 255, 0) if label == "Menggunakan Masker" else (0, 0, 255)
+
+    percent = "{:.2f}".format(max(mask, without_mask) * 100)
+    label = f"{label}: {percent}%"
+
+    cv2.putText(frame, label, (x, y - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+    cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
   
-  cv2.imshow("Face Detector", frame)
-  if cv2.waitKey(25) & 0XFF == ord('q'):
+  cv2.imshow('Capture', frame)
+  key = cv2.waitKey(20)
+  if key == 27:
     break
 
-videoCapture.release()
-cv2.destroyAllWindow()
+video_capture.release()
+cv2.destroyAllWindows()
